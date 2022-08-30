fe64071ac2d9ea96f7a8eaff4da1227ec3b74cc3
Kurt Bixby <kurtbixby@mac.com>
2022-08-30 01:53:38 -0500

    Tailwind working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { render } from "react-dom";
 const App = () => {
   return (
     <div>
-      <h1>Hello React World!</h1>
+      <h1 className="text-green-700 text-6xl">Hello React World!</h1>
     </div>
   )
 }
